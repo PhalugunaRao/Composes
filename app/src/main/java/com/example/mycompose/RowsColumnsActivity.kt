03b54d63aefbe3fa57f5493cd25b4cc7184eb350
PhalgunaRao <phalu900@gmail.com>
2021-09-18 12:25:48 +0530

    Adding column and scrollable

--- app/src/main/java/com/example/mycompose/RowsColumnsActivity.kt
+++ app/src/main/java/com/example/mycompose/RowsColumnsActivity.kt
@@ -3,33 +3,72 @@ package com.example.mycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class RowsColumnsActivity:ComponentActivity() {
+class RowsColumnsActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Green),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Bottom
-                ) {
-                Text("Hello")
-                Text( "world")
+            val scrollState = rememberScrollState()
+            // Smooth scroll to specified pixels on first composition
+            LaunchedEffect(Unit) { scrollState.animateScrollTo(10000) }
+            Column(
+                modifier = Modifier
+                    .background(Color(0xFFEDEAE0))
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .verticalScroll(scrollState)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.happy_meal_small),
+                    contentDescription ="",
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+
+                )
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "Happy Meal",
+                        style = TextStyle(
+                            fontSize = 26.sp,
+                            color = Color.Red
+
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(text = "800 Calories",
+                        style = TextStyle(
+                        fontSize = 17.sp,
+                        color = Color.Green
+
+                    ))
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(text = "$5.99",
+                        style = TextStyle(
+                            fontSize = 17.sp,
+                            color = Color(0xFF85bb65)
+
+                        ))
+
+                }
             }
-           /* Row{
-                Text("Hello")
-                Text( "world")
-            }*/
 
         }
     }
