f241843bbcf0de8fd1de68bd122bbfdb60639c51
PhalgunaRao <phalu900@gmail.com>
2021-09-20 23:21:29 +0530

    State of color

---
+++ app/src/main/java/com/example/mycompose/UiStatte.kt
@@ -0,0 +1,51 @@
+package com.example.mycompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
+
+class UiStatte:ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(Modifier.fillMaxSize()) {
+                ColorBox(
+                    Modifier.fillMaxSize().weight(1f)
+                )
+
+            }
+
+        }
+    }
+}
+@Composable
+fun ColorBox(modifier: Modifier){
+    val color= remember {
+        mutableStateOf(Color.Green)
+    }
+    Box(modifier = modifier
+        .background(color.value)
+        .clickable {
+            color.value = Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            )
+
+        }
+
+    )
+    
+}
